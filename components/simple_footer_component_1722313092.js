e0bcb72b3bf134acd5339ae9a18bbb361c47a9a1
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-30 04:18:39 +0000

    Placeholder text replacement

--- components/simple_footer_component_1722313092.js
+++ components/simple_footer_component_1722313092.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722313092", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            FoodCraft: Culinary Creativity
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        FoodCraft is a delightful app designed for food lovers and home chefs. Explore a world of recipes, create your own culinary masterpieces, and share your passion for cooking with a vibrant community. Cook with passion, eat with joy!
                     </div>
                 </div>
             </div>
